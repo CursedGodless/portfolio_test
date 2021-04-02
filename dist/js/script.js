edd671587ee9c6ef5d9c9ef7ec5c07dc36603f60
Artyom <saaworking@gmail.com>
2021-04-02 21:46:49 +0300

    Added portfolio section

--- dist/js/script.js
+++ dist/js/script.js
@@ -8,4 +8,11 @@ hamburger.addEventListener('click', () => {
 
 closeElem.addEventListener('click', () => {
   menu.classList.remove('active');
-})
+});
+
+const percents = document.querySelectorAll('.skills__percentage-percent'),
+  orange = document.querySelectorAll('.skills__percentage-bar_orange');
+
+percents.forEach((item,i) => {
+  orange[i].style.width = item.innerHTML;
+});
